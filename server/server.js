3b792b8b58a7af0e618aebd9d1fd4387ebc6ad3b
Elwin <elwin.carlos09@gmail.com>
2021-03-04 14:45:54 +0000

    add basic skeleton to server file and export functions

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
+//Create the server!
+
 const http = require('http');
+let AllAnimals;
 
 const requestListener = (request, response) => {
+  let body;
+  let statusCode;
     response.statusCode = 200;
     response.end('Hello, client!');
   };
 
-const host = 'localhost';
-const port = 5000;
+
 
 const server = http.createServer(requestListener);
-server.listen(port, host, () => console.log(`Flying pigs can now be found on http://${host}:${port}!`));
+
+const startServer = (port, host) => server.listen(port, host, () => console.log(`Super cool animals can now be found on http://${host}:${port}!`));
+
+const closeServer = cb => server.close(cb);
+
+module.exports = { startServer, closeServer};
