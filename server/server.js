5e527eeae9cc2af142fca88b4ad5c17dd5445b72
Elwin <elwin.carlos09@gmail.com>
2021-03-04 15:09:44 +0000

    add default case to switch statement!

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 //Create the server!
 
 const http = require('http');
-let AllCats = [{id: 'Mufasa', color: 'ginger'}, {id: 'Scar', color: 'black'}];
+let allCats = [{id: 'Mufasa', color: 'ginger'}, {id: 'Scar', color: 'black'}];
 let allDogs = [{id: 'Scooby Doo', color: 'brown'}, {id: 'Scar', color: 'black'}];
 let allUnicorns = [{id: 'Rainbow Dash', color: 'multi'}, {id: 'Pegasus', color: 'white'}];
 
 const requestListener = (request, response) => {
-
   let body;
   let statusCode;
   // response.statusCode = 200;
@@ -26,6 +25,10 @@ const requestListener = (request, response) => {
       if(request.method === "GET") {
         body = { unicorns: AllUnicorns }
       }; break;
+    default:
+      statusCode = 404;
+      body = { error: `Route ${req.url} does not exist`}
+      break;
 
     //Define final behaviour of requestListener function
     response.statusCode = statusCode || 200;
@@ -37,7 +40,6 @@ const requestListener = (request, response) => {
 const server = http.createServer(requestListener);
 
 const startServer = (port, host) => server.listen(port, host, () => console.log(`Super cool animals can now be found on http://${host}:${port}!`));
-
 const closeServer = cb => server.close(cb);
 
 module.exports = { startServer, closeServer};
